90d2de731d0847cacacf6e47167ce6589c111562
Cloud9 <waiguaishushu@foxmail.com>
2022-04-28 13:10:24 +0800

    图表页面数据接口完成

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -8,20 +8,62 @@ axios.get('/student/overview').then(({ data: res }) => {
     document.querySelector('.proportion').innerHTML = data.proportion
   }
 })
+// 统计省份/地区人数,保存为echarts需要的格式
+function area(arr) {
+  const obj = {}
+  arr.forEach((item) => {
+    obj[item] = obj[item] ? obj[item] + 1 : 1
+  })
+  // console.log(obj)
+  const newArr = []
+  for (let k in obj) {
+    let item = {}
+    item.value = obj[k]
+    item.name = k
+    newArr.push(item)
+  }
+  return newArr
+}
+// 统计各户籍地、经纬度,保存为echarts需要的格式
+function homeTown(home) {
+  // 保存户籍地经纬度信息
+  const coords = {}
+  // 保存户籍地人数信息
+  const num = []
+  const obj = {}
+  home.forEach((item) => {
+    obj[item[0]] = obj[item[0]] ? obj[item[0]] + 1 : 1
+    coords[item[0]] = item[1]
+  })
+  // console.log(obj)
+  // console.log(coords)
+  for (let k in obj) {
+    let item = {}
+    item.value = obj[k]
+    item.name = k
+    num.push(item)
+  }
+  return [coords, num]
+}
 // 获取学员信息
 axios.get('student/list').then(({ data: res }) => {
   const names = []
   const salarys = []
   const truesalarys = []
   const areas = []
+  const home = []
   res.data.forEach((item) => {
     names.push(item.name)
     salarys.push(item.salary)
     truesalarys.push(item.truesalary)
     areas.push(item.province)
+    home.push([item.city, [item.jing, item.wei]])
   })
+  // console.log(areas)
+  // console.log(home)
   lineChart(names, salarys, truesalarys)
-  pieChart(homeTown(areas))
+  pieChart(area(areas))
+  mapChart(homeTown(home))
 })
 // 第n次成绩选择
 $('.btn').on('click', function () {
@@ -47,22 +89,7 @@ $('#batch li').on('click', function () {
 })
 // 页面加载后，触发第一个li的单击事件
 $('#batch li').eq(0).trigger('click')
-// 计算省份/地区各自比例,保存为echarts需要的格式
-function homeTown(arr) {
-  const obj = {}
-  arr.forEach((item) => {
-    obj[item] = obj[item] ? obj[item] + 1 : 1
-  })
-  // console.log(obj)
-  const newArr = []
-  for (let k in obj) {
-    let item = {}
-    item.value = obj[k]
-    item.name = k
-    newArr.push(item)
-  }
-  return newArr
-}
+
 // 薪水折线图
 function lineChart(names, salarys, truesalarys) {
   const option = {
@@ -312,8 +339,190 @@ function pieChart(datas) {
 }
 // pieChart()
 // 地图
-function mapChart() {
-  const option = {}
+function mapChart(datas) {
+  var geoCoordMap = datas[0]
+  // console.log(datas)
+  var dataT = datas[1]
+  // 中心点
+  geoCoordMap['北京'] = [116.4551, 40.2539]
+
+  var GZData = []
+  var tempData = ['北京', GZData]
+
+  dataT.map((item, index) => {
+    var arr = []
+    arr.push(item)
+    arr.push({
+      name: '北京',
+    })
+    GZData.push(arr)
+  })
+
+  var convertData = function (data) {
+    var res = []
+    for (var i = 0; i < data.length; i++) {
+      var dataItem = data[i]
+      var fromCoord = geoCoordMap[dataItem[0].name]
+      var toCoord = geoCoordMap[dataItem[1].name]
+      if (fromCoord && toCoord) {
+        res.push({
+          fromName: dataItem[0].name,
+          toName: dataItem[1].name,
+          coords: [fromCoord, toCoord],
+          value: dataItem[0].value,
+        })
+      }
+    }
+    return res
+  }
+
+  var color = ['#00ff78', '#ff971b', '#acff43', '#ff73b7', '#ffff46', '#27bbfe']
+  var series = []
+  series.push(
+    {
+      name: tempData[0],
+      type: 'lines',
+      zlevel: 3,
+      symbol: ['none', 'circle'],
+      effect: {
+        show: true,
+        period: 3, //箭头指向速度，值越小速度越快
+        trailLength: 0.01, //特效尾迹长度[0,1]值越大，尾迹越长重
+        symbolSize: 4, //图标大小
+      },
+      lineStyle: {
+        normal: {
+          opacity: 1,
+          curveness: 0.4, //曲线的弯曲程度
+          color: '#609fd4',
+        },
+      },
+      data: convertData(tempData[1]),
+    },
+    {
+      name: tempData[0],
+      type: 'scatter',
+      coordinateSystem: 'geo',
+      zlevel: 2,
+      rippleEffect: {
+        brushType: 'stroke',
+      },
+      label: {
+        normal: {
+          show: true,
+          position: 'right',
+          formatter: '{b}',
+        },
+      },
+      symbolSize: function (val) {
+        console.log(val)
+        return val[2] / 2
+      },
+      itemStyle: {
+        normal: {
+          //                fontSize: 80,
+        },
+      },
+      data: tempData[1].map(function (dataItem) {
+        return {
+          name: dataItem[0].name,
+          value: geoCoordMap[dataItem[0].name].concat([dataItem[0].value]),
+        }
+      }),
+    },
+    {
+      // 地图气泡
+      type: 'effectScatter',
+      coordinateSystem: 'geo',
+      zlevel: 4,
+      symbolSize: 10, //标记大小
+      label: {
+        normal: {
+          show: true,
+          position: 'right',
+          formatter: '{b}',
+
+          color: 'white',
+        },
+      },
+      itemStyle: {
+        normal: {
+          color: 'white',
+        },
+      },
+      rippleEffect: {
+        scale: 4,
+        brushType: 'stroke',
+      },
+      data: [
+        {
+          name: tempData[0],
+          value: geoCoordMap[tempData[0]],
+          visualMap: false,
+        },
+      ],
+    }
+  )
+
+  const option = {
+    tooltip: {
+      trigger: 'item',
+      formatter: function (params) {
+        if (params.seriesType == 'scatter' && params.name != tempData[0]) {
+          return (
+            '<br>' +
+            params.seriesName +
+            ' ---> ' +
+            params.data.name +
+            '<br />数量：' +
+            params.data.value[2]
+          )
+        } else if (params.seriesType == 'lines') {
+          return (
+            '<br>' +
+            params.data.fromName +
+            ' ---> ' +
+            params.data.toName +
+            '<br />数量：' +
+            params.data.value
+          )
+        } else {
+          return params.name
+        }
+      },
+    },
+    visualMap: {
+      min: 0,
+      max: 100,
+      calculable: true,
+      color: color,
+      //            textStyle: {
+      //                color: '#fff',
+      //            },
+      show: false,
+    },
+    geo: {
+      map: 'china',
+      zoom: 0.98,
+      zlevel: 2,
+      label: {
+        emphasis: {
+          show: false,
+        },
+      },
+      roam: true, //是否允许缩放
+      itemStyle: {
+        normal: {
+          areaColor: '#214278',
+          borderColor: '#195BB9', //省市边界线00fcff 516a89
+        },
+        emphasis: {
+          color: '#112246', //悬浮背景
+        },
+      },
+    },
+    series: series,
+  }
   echarts.init(document.querySelector('.map')).setOption(option)
 }
-mapChart()
+// mapChart()
